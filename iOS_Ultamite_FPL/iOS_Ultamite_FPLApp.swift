6307504e780f7408f770264b0d4b13a3f89dfef8
MohammadBakeer <159083259+MohammadBakeer@users.noreply.github.com>
2024-10-11 16:22:50 -0500

    Initial Commit

---
+++ iOS_Ultamite_FPL/iOS_Ultamite_FPLApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Ultamite_FPLApp.swift
+//  iOS_Ultamite_FPL
+//
+//  Created by Mohammad Bakeer on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Ultamite_FPLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
